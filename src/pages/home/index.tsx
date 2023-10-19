743c89e72ae53411d9bede607b0fa474641c6cfc
felipesavdev <felipesavdev@gmail.com>
2023-10-19 15:34:40 -0300

    feat/banner

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import Header from "../../components/templates/header";
 import Banner from "../../components/ui/banner";
-import Buttons from "../../components/ui/buttons";
 
 const HomePage = () => {
-	const navigate = useNavigate();
+	
 		return (
 		<>
-			<Header />
-			<button onClick={() => navigate('/contact')}>
-				Contact
-			</button>             
+			<Header />         
 			<Banner /> 
-			<Buttons/>
 		</>
 	)
 }

bf333f018cc7dd811508bd2b3a4206f7b7d3f528
felipesavdev <felipesavdev@gmail.com>
2023-10-24 18:41:12 -0300

    refactor: ultimas-atuliazações-concluidas

--- src/components/ui/banner/index.tsx
+++ src/components/ui/banner/index.tsx
@@ -1,37 +1,35 @@
 import './styles.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-function Banner() {
+const Banner = () => {
   const data = [
     { 
-      id: '1', 
       image: './src/assets/images/banners/m4a1s.jpg',
       name: 'm4a1s' 
     },
     { 
-      id: '2', 
       image: './src/assets/images/banners/ak47.jpg',
-      name: ''
+      name: 'ak47'
     },
     { 
-      id: '3', 
       image: './src/assets/images/banners/usp.jpg',
       name: 'usp'
     }, 
   ];
 
   return (
-  <div className="banner-container">
-    <Swiper slidesPerView={1} navigation>
-      {data.map((item) => {
-        return (
-          <SwiperSlide key={item.id}>
-            <img src={item.image} alt={item.name} className="slide-item" />
-          </SwiperSlide>
-        )
-      })}
-    </Swiper>
-  </div>
-  )};
+    <div className="banner-container">
+      <Swiper slidesPerView={1} navigation>
+        {data.map((item, index) => {
+          return (
+            <SwiperSlide key={index} className="swiper-slide">
+              <img src={item.image} alt={item.name} className="slide-item" />
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </div>
+  );
+}
 
 export default Banner;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,15 @@
 import Header from "../../components/templates/header";
 import Banner from "../../components/ui/banner";
-
+import './styles.scss'
 const HomePage = () => {
 	
 		return (
 		<>
-			<Header />         
-			<Banner /> 
+			<Header />      
+			<div className="content">
+				<Banner />
+			</div>  
+			 
 		</>
 	)
 }

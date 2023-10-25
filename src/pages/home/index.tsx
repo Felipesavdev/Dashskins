36dfcd50f963ebafe835e05bd6f8be63e7384e6a
felipesavdev <felipesavdev@gmail.com>
2023-10-25 14:29:26 -0300

    refactor:correçoes-finalizadas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,15 @@
 import Header from "../../components/templates/header";
 import Banner from "../../components/ui/banner";
 import './styles.scss'
+
 const HomePage = () => {
-	
-		return (
+	return (
 		<>
-			<Header />      
+			<Header />
 			<div className="content">
 				<Banner />
-			</div>  
-			 
+			</div>
 		</>
-	)
+	);
 }
-
 export default HomePage;
